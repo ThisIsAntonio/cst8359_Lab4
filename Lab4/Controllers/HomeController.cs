201494f628518955d349389868d41c87da739ff6
Marcos Astudillo <antonio.ac86@hotmail.com>
2024-05-31 17:32:09 -0400

    Completed step 3: Added controllers and views for HomeController

---
+++ Lab4/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+            int numberOfMonkeys = int.Parse(Request.Form["numberOfMonkeys"]);
+            ViewBag.NumberOfMonkeys = numberOfMonkeys;
+
+            return View();
+        }
+
+        public IActionResult CreateStudent() => View();
+
+        [HttpPost]
+        public IActionResult DisplayStudent(Student student)
+        {
+            return View(student);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
